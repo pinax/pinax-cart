75b53f45124b2ae0056608524c789784d8423c60
Brian Rosner <brian@brosner.com>
2015-12-08 13:55:03 -0700

    Fixed setup.py to pass test setup

--- setup.py
+++ setup.py
@@ -1,21 +1,45 @@
-import codecs
-
-from os import path
 from setuptools import find_packages, setup
 
 
-def read(*parts):
-    filename = path.join(path.dirname(__file__), *parts)
-    with codecs.open(filename, encoding="utf-8") as fp:
-        return fp.read()
+LONG_DESCRIPTION = """
+==========
+Pinax Cart
+==========
+.. image:: http://slack.pinaxproject.com/badge.svg
+    :target: http://slack.pinaxproject.com/
+.. image:: https://img.shields.io/travis/pinax/pinax-cart.svg
+    :target: https://travis-ci.org/pinax/pinax-cart
+.. image:: https://img.shields.io/coveralls/pinax/pinax-cart.svg
+    :target: https://coveralls.io/r/pinax/pinax-cart
+.. image:: https://img.shields.io/pypi/dm/pinax-cart.svg
+    :target:  https://pypi.python.org/pypi/pinax-cart/
+.. image:: https://img.shields.io/pypi/v/pinax-cart.svg
+    :target:  https://pypi.python.org/pypi/pinax-cart/
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target:  https://pypi.python.org/pypi/pinax-cart/
+
+Pinax
+------
+
+Pinax is an open-source platform built on the Django Web Framework. It is an
+ecosystem of reusable Django apps, themes, and starter project templates.
+This collection can be found at http://pinaxproject.com.
+This app was developed as part of the Pinax ecosystem but is just a Django app
+and can be used independently of other Pinax apps.
+
+pinax-cart
+----------
+
+`pinax-cart` is a shopping cart app for Django.
+"""
 
 
 setup(
-    author="",
-    author_email="",
-    description="",
+    author="Pinax Team",
+    author_email="team@pinaxproject.com",
+    description="a shopping cart app for Django",
     name="pinax-cart",
-    long_description=read("README.rst"),
+    long_description=LONG_DESCRIPTION,
     version="0.1",
     url="http://github.com/pinax/pinax-cart/",
     license="MIT",
